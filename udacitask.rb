deee6f5beb6fe6c1af8a17164262178c6426eb1e
Mert ARISOY <arisoymert@gmail.com>
2016-02-04 20:34:11 +0200

    first commit

---
+++ udacitask.rb
@@ -0,0 +1,32 @@
+require_relative 'todolist.rb'
+
+# Creates a new todo list
+
+# Add four new items
+todo1 = Item.new("Clean the house")
+todo2 = Item.new("Go to market")
+todo3 = Item.new("Give the budget report to the boss")
+todo4 = Item.new("Wash the car")
+
+# Print the list
+puts todo1
+puts todo2
+puts todo3
+puts todo4
+
+# Delete the first item
+todo1.remove_item
+
+# Print the list
+
+# Delete the second item
+
+# Print the list
+
+# Update the completion status of the first item to complete
+
+# Print the list
+
+# Update the title of the list
+
+# Print the list
